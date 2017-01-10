160082019d88468c7e35697098392eeee0995c69
Sudhir <sudhir@indusos.com>
2017-01-10 22:03:13 +0530

    fixed bugs related to html encode, language specific apostrophe problems etc

---
+++ lang_constants.php
@@ -0,0 +1,16 @@
+<?php 
+define('hi_CHARS', "\x{0900}-\x{0963}\x{0966}-\x{0971}");
+define('ta_CHARS', "\x{0B80}-\x{0BFF}");
+define('ml_CHARS', "\x{0D00}-\x{0D7F}");
+define('te_CHARS', "\x{0C00}-\x{0C7F}");
+define('kn_CHARS', "\x{0C80}-\x{0CFF}");
+define('mr_CHARS', "\x{0900}-\x{0963}\x{0966}-\x{0972}");
+define('or_CHARS', "\x{0B00}-\x{0B7F}");
+define('gu_CHARS', "\x{0A80}-\x{0AFF}");
+define('bn_CHARS', "\x{0980}-\x{09EF}\x{09FA}");
+define('as_CHARS', "\x{0980}-\x{09FF}");
+define('ur_CHARS', "\x{0600}-\x{097F}");
+define('pa_CHARS', "\x{0A00}-\x{0A7F}");
+define('apostrophe_CHARS', "\x{0027}\x{05F3}\x{2019}");
+define('colon_CHARS', ":");
+?> 
